4bcdaebab9d668c8e6f4c26ec2b316f7e9d47aad
apple <153444073@qq.com>
2019-04-17 09:53:54 +0800

    Fix

--- routes/jiazhengApp.js
+++ routes/jiazhengApp.js
@@ -167,5 +167,19 @@ router.post('/pingjia', function(req, res, next) {
   
 
 });
+//拿取二维码
+router.post('/getSQRR', function(req, res, next) {
+  var dic = req.body;
+ fs.readFile(__dirname + '/jisudaApp/SQRsuccess.json', function(err, data){
+  if(!err){
+    // res.writeHead(200, {"Content-Type": "text/json;charset=UTF-8"});
+     console.log(data);
+      res.send(data);
+  }else{
+      throw err;
+  }
+})
+});
+
 
 module.exports = router;
